4eeffb4390dd71580bfbefc8eaca241aa0bcf65f
SnehaMane17 <snehamane171998@gmail.com>
2022-07-18 15:19:07 +0530

    chart displayed successfully

--- script.js
+++ script.js
@@ -9,35 +9,31 @@ function generatePDF() {
 
 //Chart Generation
 
-var chart, 
-  options = { 
-    debug: true, 
-    type: 'column', 
-    title_label_text: 'Take HTML and Chart it.', 
-    defaultPoint_label_visible: true
-  }; 
-  
-JSC.ready().then(function() { 
-  options.series = [ 
-    getData('table', 'States') 
-  ]; 
-  chart = new JSC.Chart('chartDiv', options); 
-}); 
-  
-function getData(tableId, seriesName) { 
-  var series = { points: [], name: seriesName }; 
-  var rowItems = document.querySelectorAll( 
-    '#dataTable tr'
-  ); 
-  for (var i = 0; i < rowItems.length; i++) { 
-    var rowItem = rowItems[i]; 
-    var tds = rowItem.children; 
-    var point = [ 
-      tds[0].innerText, 
-      parseInt(tds[1].innerText) 
-    ]; 
-    series.points.push(point); 
-  } 
-  
-  return series; 
-} 
+function generateChart(){
+    Highcharts.chart('container', {
+        data: {
+          table: 'datatable',
+          startRow: 0,
+          startColumn: 0,
+          endColumn: 1
+        },
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: 'Data extracted from a HTML table in the page'
+        },
+        yAxis: {
+          allowDecimals: false,
+          title: {
+            text: 'Units'
+          }
+        },
+        tooltip: {
+          formatter: function () {
+            return '<b>' + this.series.name + '</b><br/>' +
+              this.point.y + ' ' + this.point.name.toLowerCase();
+          }
+        }
+      });
+}
